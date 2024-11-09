f663e346e5630168792fdd12d6409f07a94f7bfe
Jin Choi <choij@cern.ch>
2024-11-09 16:36:50 +0100

    regular sync

--- copy_index.py
+++ copy_index.py
@@ -11,11 +11,12 @@ def copy_recursively(path, base_path="."):
     path = f"{base_path}/{path}"
     os.system(f"cp index.php {path}")
     sub_paths = [x for x in os.listdir(path) if os.path.isdir(f"{path}/{x}")]
-    
+
     if len(sub_paths) == 0:
         return
     else:
         for p in sub_paths:
+            if p == "myblog": continue
             copy_recursively(p, path)
 
 if __name__ == "__main__":
